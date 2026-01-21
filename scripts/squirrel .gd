b64ca705c13cbf113c3e9ea4c57670c8fbc1d962
Hello6012 <avi.sigel@gmail.com>
2026-01-20 19:26:03 -0600

    seperated levels

--- scripts/squirrel .gd
+++ scripts/squirrel .gd
@@ -8,11 +8,4 @@ var direction = 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if ray_cast_right.is_colliding():
-		direction = -1
-		animated_sprite.flip_h = true
-	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite.flip_h = false
-	
-	position.x += direction * SPEED * delta
+	position.y += direction * SPEED * delta
