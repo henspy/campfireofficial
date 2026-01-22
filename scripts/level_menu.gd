eb68278e3bd6ace69bf9a5fffdb0277698124117
Hello6012 <avi.sigel@gmail.com>
2026-01-22 17:21:20 -0600

    made level menu

---
+++ scripts/level_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
+
+
+func _on_level_3_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
