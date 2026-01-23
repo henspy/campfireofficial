112055afa77f8a6bc6710a34875d6eae215cf8ba
Hello6012 <avi.sigel@gmail.com>
2026-01-23 17:24:38 -0600

    fixed anchoring issues in menus

--- scripts/level_menu.gd
+++ scripts/level_menu.gd
@@ -17,3 +17,7 @@ func _on_level_2_pressed() -> void:
 
 func _on_level_3_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_3.tscn")
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/mainMenu.tscn")
--- scripts/volume_menu.gd
+++ scripts/volume_menu.gd
@@ -15,3 +15,7 @@ func _on_confirm_pressed() -> void:
 	AudioServer.set_bus_volume_db(0, linear_to_db($"audio options/VBoxContainer/master".value))
 	AudioServer.set_bus_volume_db(1, linear_to_db($"audio options/VBoxContainer/music".value))
 	AudioServer.set_bus_volume_db(2, linear_to_db($"audio options/VBoxContainer/sfx".value))
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/options_menu.tscn")
